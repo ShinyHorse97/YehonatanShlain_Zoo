0ee302d49310d4b97386d86f04b45c5086120dfb
ShinyHorse97 <jonatan.shlain@outlook.com>
2025-09-17 09:19:53 +0300

    Class Mammal

--- Mammal.cs
+++ Mammal.cs
@@ -10,9 +10,18 @@ namespace YehonatanShlain_Zoo
     {
         private double CalInMilk;
         private int PregnancyDuration;
-        public Mammal()
+        public Mammal(double CalInMilk, int PregnancyDuration, string name, string gender, double calories, bool predator, Date dateOfBirth) : base(name, gender, calories, predator, dateOfBirth)
         {
-
+            this.CalInMilk = CalInMilk;
+            this.PregnancyDuration = PregnancyDuration;
+        }
+        public double GetCalInMilk() { return CalInMilk; }
+        public int GetPregnancyDuration() { return PregnancyDuration; }
+        public void SetCalInMilk(double calInMilk) { CalInMilk = calInMilk; }
+        public void SetPregnancyDuration(int pregnancyDuration) { PregnancyDuration = pregnancyDuration; }
+        public override string ToString()
+        {
+            return base.ToString() + ", Calories in milk: " + CalInMilk + ", Pregnancy duration: " + PregnancyDuration;
         }
     }
 }
